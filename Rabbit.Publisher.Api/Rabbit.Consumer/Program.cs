993793fa930a93ea36fc5dfaf8bada47441e9987
marciomonzon <marcio.pcmonzon@gmail.com>
2022-10-21 20:58:34 -0300

    consumer implemented

--- Rabbit.Publisher.Api/Rabbit.Consumer/Program.cs
+++ Rabbit.Publisher.Api/Rabbit.Consumer/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Rabbit.Model.Entities;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory() 
+{ 
+    HostName = "localhost",
+    UserName = "admin",
+    Password = "123456"
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "RabbitMessagesQueue",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var jsonMessage = Encoding.UTF8.GetString(body);
+
+        RabbitMessage rabbitMessage = JsonSerializer.Deserialize<RabbitMessage>(jsonMessage);
+
+        Console.WriteLine($"Titulo: {rabbitMessage.Titulo}; Texto: {rabbitMessage.Texto}; Id: {rabbitMessage.Id}");
+    };
+
+    channel.BasicConsume(queue: "RabbitMessagesQueue",
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
